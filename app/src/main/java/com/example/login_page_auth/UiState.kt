ab3ed897b41c8d2f479f24a85e94c0cc140d9c5e
Adnurhytia <adindanurhayati30@gmail.com>
2024-01-09 01:33:14 +0700

    Menambahkan isi UiState

--- app/src/main/java/com/example/login_page_auth/UiState.kt
+++ app/src/main/java/com/example/login_page_auth/UiState.kt
@@ -1,5 +1,7 @@
 package com.example.login_page_auth
 
+import com.example.login_page_auth.model.Pet
+
 data class AddUIState(
     val addEvent: AddEvent = AddEvent(),
 )
@@ -7,6 +9,32 @@ data class AddUIState(
 data class AddEvent(
     val id: String ="",
     val namapet: String = "",
-    val jenidpet: String = "",
+    val jenispet: String = "",
     val telpon: String = "",
+)
+
+fun AddEvent.toPet() = Pet(
+    id = id,
+    namapet = namapet,
+    jenispet = jenispet,
+    telpon = telpon
+)
+
+data class DetailUIState(
+    val addEvent: AddEvent = AddEvent(),
+)
+
+fun Pet.toDetailPet(): AddEvent =
+    AddEvent(
+        id = id,
+        namapet = namapet,
+        jenispet = jenispet,
+        telpon = telpon
+    )
+fun  Pet.toUIStatePet(): AddUIState = AddUIState(
+    addEvent = this.toDetailPet()
+)
+data class HomeUIState(
+    val listPet: List<Pet> = listOf(),
+    val datalength: Int =0
 )
