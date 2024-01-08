5a68dd847f27e5c6715905f50b23c60b892d1f74
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2024-01-09 03:00:09 +0700

    create class ApplicationFirebaseAuthentiaction App for MainAct

--- app/src/main/java/com/example/login_page_auth/FirebaseAuthenticationApp.kt
+++ app/src/main/java/com/example/login_page_auth/FirebaseAuthenticationApp.kt
@@ -1,9 +1,17 @@
 package com.example.login_page_auth
 
 import android.app.Application
+import com.example.login_page_auth.repositori.AppContainer
+import com.example.login_page_auth.repositori.PetContainer
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class FirebaseAuthenticationApp: Application() {
+    lateinit var container: AppContainer
 
+    override fun onCreate(){
+        super.onCreate()
+
+        container = PetContainer()
+    }
 }
