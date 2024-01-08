83bcf5746075b41d113cac76466ea284eb3d70d0
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2024-01-09 03:05:01 +0700

    create class HomeViewModel

--- app/src/main/java/com/example/login_page_auth/home/HomeViewModel.kt
+++ app/src/main/java/com/example/login_page_auth/home/HomeViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.login_page_auth.home
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.login_page_auth.HomeUIState
 import com.example.login_page_auth.model.Pet
+import com.example.login_page_auth.repositori.PetRepositori
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 
 sealed class PetUIState{
@@ -9,6 +18,21 @@ sealed class PetUIState{
     object Error : PetUIState()
     object  Loading : PetUIState()
 }
-class HomeViewModel {
+class HomeViewModel(private val petRepositori: PetRepositori) : ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUIState: StateFlow<HomeUIState> = petRepositori.getAll()
+        .filterNotNull()
+        .map {
+            HomeUIState (listPet = it.toList(), it.size ) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUIState()
+
+        )
 
 }
