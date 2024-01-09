4b45fd418438bb569470fc0b265d0cb3569859d1
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2024-01-09 23:10:52 +0700

    create new object for DestinasiAdd

--- app/src/main/java/com/example/login_page_auth/add/AddData.kt
+++ app/src/main/java/com/example/login_page_auth/add/AddData.kt
@@ -1,3 +1,6 @@
+import com.example.login_page_auth.navigation.DestinasiNavigasi
 
-
-object DestinasiEntry : DestinasiNavigasi
+object DestinasiAdd : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Siswa"
+}
