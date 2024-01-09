ec506a3c4c2110de2c9cc19e13284a4bffae1850
Adnurhytia <adindanurhayati30@gmail.com>
2024-01-10 00:46:48 +0700

    Add detail view model

--- app/src/main/java/com/example/login_page_auth/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/example/login_page_auth/ui/detail/DetailScreen.kt
@@ -1,2 +1,10 @@
 package com.example.login_page_auth.ui.detail
 
+import com.example.login_page_auth.navigation.DestinasiNavigasi
+
+object DetailDestination : DestinasiNavigasi {
+    override val route = "item_details"
+    override val titleRes = "Detail Pet"
+    const val petId = "itemId"
+    val routeWithArgs = "$route/{$petId}"
+}
