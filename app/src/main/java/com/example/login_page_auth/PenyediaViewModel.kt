c394b09e13645c6f999fe77afd1b38dc677a932a
Adnurhytia <adindanurhayati30@gmail.com>
2024-01-09 15:30:24 +0700

    create FirebaseAuthenticationApp for PenyediaViewModel.kt

--- app/src/main/java/com/example/login_page_auth/PenyediaViewModel.kt
+++ app/src/main/java/com/example/login_page_auth/PenyediaViewModel.kt
@@ -1,2 +1,7 @@
 package com.example.login_page_auth
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+
+fun CreationExtras.FirebaseAuthenticationApp(): FirebaseAuthenticationApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as FirebaseAuthenticationApp)
