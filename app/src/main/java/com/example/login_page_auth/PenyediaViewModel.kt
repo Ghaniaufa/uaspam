9a0f9fdbd4509beabc0fcd3894c689d4a3a8cdc4
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2024-01-09 18:48:32 +0700

    add initializer for HomeViewModel at PenyediaViewModel.kt

--- app/src/main/java/com/example/login_page_auth/PenyediaViewModel.kt
+++ app/src/main/java/com/example/login_page_auth/PenyediaViewModel.kt
@@ -2,6 +2,21 @@ package com.example.login_page_auth
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.login_page_auth.home.HomeViewModel
+import com.example.login_page_auth.login.IgViewModel
 
-fun CreationExtras.FirebaseAuthenticationApp(): FirebaseAuthenticationApp =
+fun CreationExtras.aplikasiPet(): FirebaseAuthenticationApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as FirebaseAuthenticationApp)
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiPet().container.petRepositori)
+        }
+    }
+
+
+}
