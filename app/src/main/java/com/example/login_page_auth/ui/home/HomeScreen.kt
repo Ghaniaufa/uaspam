55df631664eb222e3b921ec705616e3d1aec8c12
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2024-01-11 02:48:39 +0700

    fix the design for HomeScreen

--- app/src/main/java/com/example/login_page_auth/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/login_page_auth/ui/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.login_page_auth.ui.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,19 +11,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.login_page_auth.R
 import com.example.login_page_auth.ui.login.IgViewModel
 import com.example.login_page_auth.navigation.DestinasiNavigasi
 import com.example.login_page_auth.navigation.DestinationScreen
@@ -34,53 +41,50 @@ import com.example.login_page_auth.navigation.DestinationScreen
 fun HomeScreen(navController: NavController) {
    // Set background color for the entire screen
    Box(
+
       modifier = Modifier
-         .clip(RoundedCornerShape(50.dp))
-         .background(Color(255, 122, 122, 255))
+         .background(Color(255, 206, 206, 255))
          .fillMaxSize()
    ) {
+
       Column(
          verticalArrangement = Arrangement.Center,
          modifier = Modifier
+
             .fillMaxSize()
             .padding(16.dp)
       ) {
-         Column(
-            verticalArrangement = Arrangement.Center,
+         SmallCard1(navController = navController, title = "Pet Hotel" , )
+
+         Spacer(modifier = Modifier.height(16.dp))
+         // Large card at the top
+         SmallCard2(navController = navController, title = "Pet Hotel")
+
+
+         // Spacer between large card and small cards
+         Spacer(modifier = Modifier.height(16.dp))
+
+         SmallCard3(navController = navController, title = "All Pet Data")
+         // Two small cards below, arranged horizontally
+         Row(
             modifier = Modifier
-               .fillMaxSize()
-               .padding(16.dp)
+               .fillMaxWidth()
+               .padding(top = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
          ) {
-            // Large card at the top
-            LargeCard("Large Card", navController = navController)
 
 
-            // Two small cards below, arranged horizontally
-            Row(
-               modifier = Modifier
-                  .fillMaxWidth()
-                  .padding(top = 16.dp),
-               horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-               SmallCard1(navController = navController, title = "All Pet Data")
-               SmallCard2(navController = navController, title = "All Product")
-            }
+
          }
       }
    }
 }
 
 @Composable
-fun LargeCard(
-   title: String,
-   navController: NavController,
-) {
+fun SmallCard1(navController: NavController, title: String) {
    Card(
       modifier = Modifier
-         .fillMaxWidth()
-         .background(Color(255, 122, 122, 255))
-         .padding(end = 10.dp) // Adjusted padding here
-         .padding(bottom = 10.dp), // Added padding here
+         .padding(end = 8.dp), // Adjusted padding here
    ) {
       Column(
          modifier = Modifier
@@ -90,25 +94,25 @@ fun LargeCard(
          verticalArrangement = Arrangement.Center
       ) {
          Text(text = title, fontWeight = FontWeight.Bold)
-         Spacer(modifier = Modifier.height(16.dp))
+         Spacer(modifier = Modifier
+
+            .height(16.dp))
          Button(
             colors = ButtonDefaults.buttonColors(
-               Color.Transparent
+               Color(255, 122, 122, 255)
             ),
-            onClick = {
-               navController.navigate("PetHotelScreen")
-            },
-         )
-         {
-            Text(text = "Pet Hotel")
+            onClick = { navController.navigate("PetHotelScreen") }
+         ) {
+            Text(text = "All Pet Data")
          }
       }
    }
 }
 @Composable
-fun SmallCard1(navController: NavController, title: String) {
+fun SmallCard2(navController: NavController, title: String) {
    Card(
       modifier = Modifier
+
          .fillMaxWidth()
          .padding(end = 8.dp), // Adjusted padding here
    ) {
@@ -123,7 +127,7 @@ fun SmallCard1(navController: NavController, title: String) {
          Spacer(modifier = Modifier.height(16.dp))
          Button(
             colors = ButtonDefaults.buttonColors(
-               Color.Transparent
+               Color(255, 122, 122, 255)
             ),
             onClick = { navController.navigate("AllDataPet") }
          ) {
@@ -134,7 +138,7 @@ fun SmallCard1(navController: NavController, title: String) {
 }
 
 @Composable
-fun SmallCard2(navController: NavController, title: String) {
+fun SmallCard3(navController: NavController, title: String) {
    Card(
       modifier = Modifier
          .fillMaxWidth()
@@ -151,7 +155,7 @@ fun SmallCard2(navController: NavController, title: String) {
          Spacer(modifier = Modifier.height(16.dp))
          Button(
             colors = ButtonDefaults.buttonColors(
-               Color.Transparent
+               Color(255, 122, 122, 255)
             ),
             onClick = { navController.navigate("Product") }
          ) {
@@ -160,6 +164,7 @@ fun SmallCard2(navController: NavController, title: String) {
       }
    }
 }
+
 
 
 
